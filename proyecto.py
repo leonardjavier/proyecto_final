4fb94dbf2711dc86c25ea945724ccb616d00e562
leonardo <leonardoabreu309@gmail.com>
2022-12-05 12:06:18 -0400

    agregacion de mas librerias, creacion del dataset, entre otras cosas.

--- proyecto.py
+++ proyecto.py
@@ -1,5 +1,15 @@
+#librerias 
+
+## sistema 
+import sys
+import os
+from glob import glob
+
+## extracion de datos
 import urllib.request, urllib.error, urllib.parse, operator
-import pandas
+
+## manipulacion de datos 
+import pandas as pd
 
 
 PATH = "https://raw.githubusercontent.com/prasertcbs/basic-dataset/master/RestaurantRating/Restaurant%20customer%20data.csv"
@@ -7,13 +17,9 @@ PATH = "https://raw.githubusercontent.com/prasertcbs/basic-dataset/master/Restau
 
 try:
     url_data = urllib.request.urlopen(PATH)
-    for linea in url_data:
-        print(linea.decode())  
+    df = pd.read_csv(url_data, index_col="userID")
+    print(df)
+
 except Exception as e:
     print(e)
-
-
-
-
-
 
