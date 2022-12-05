bb35e02b454388d7df5f38893c44573b5f90aaee
leonardo <leonardoabreu309@gmail.com>
2022-12-05 13:13:45 -0400

    filtrado de las filas y las columnas del data set.

--- proyecto.py
+++ proyecto.py
@@ -16,9 +16,27 @@ PATH = "https://raw.githubusercontent.com/prasertcbs/basic-dataset/master/Restau
 
 
 try:
+    # Solicitud de datos en la web
     url_data = urllib.request.urlopen(PATH)
-    df = pd.read_csv(url_data, index_col="userID")
-    print(df)
+
+    # Creacion del data frame
+    df_origin = pd.read_csv(url_data, index_col="userID")
+
+    # Filtrado de las columnas del data frame
+    df_filtro_columnas = df_origin[["smoker","dress_preference","drink_level","birth_year","activity","color","budget","height","latitude","longitude"]]
+    # print(df_fitro_columnas)
+
+    # Filtrado de valores 
+    df_filtros_fila= df_filtro_columnas[df_filtro_columnas["dress_preference"] != "?"]
+    print(df_filtros_fila.info())
+
+
+
+    
+
+
+
+
 
 except Exception as e:
     print(e)
